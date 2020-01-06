344baaf22687cc927cea3f773150d70efee180ba
Your Name <you@example.com>
2020-01-06 20:09:08 +0800

    样式初始化文件完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app">
     <router-view></router-view>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+      <p class="lizi">你好</p>
   </div>
 </template>
 
@@ -10,6 +16,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.lizi{
+  font-size: 15px;
+  height: 200px;
+  width: 200px;
+  background-color: pink;
+}
 </style>
