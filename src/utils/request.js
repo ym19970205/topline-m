cc38b29708f55070cba5fd0fd202c320129a2015
Your Name <you@example.com>
2020-01-10 16:52:20 +0800

    发送token-使用请求拦截器统一注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+// 这里的store和组件中的this.$store是一样的
+import store from '../store'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
@@ -14,5 +16,31 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 统一设置Token,不是组件不能直接获取vuex容器
+  const { user } = store.state
+  // 判断一下有没有token，后端要求我们放到请求头中，使用名字Authorization
+  if (user) {
+    // config.headers用来获取本次的请求头，这是axios的固定API
+    // 注意后端要求的token数据格式为 Bearer token
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 
 export default request
