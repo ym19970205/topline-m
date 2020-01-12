76752a9ba6a854070047e3920632ce52dd044c97
Your Name <you@example.com>
2020-01-12 10:54:08 +0800

    文章列表-样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
       <!--导航栏-->
-     <van-nav-bar title="首页" />
+     <van-nav-bar title="首页" fixed/>
 
       <!--频道列表-->
       <van-tabs v-model="active">
@@ -56,6 +56,28 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.home-container {
+  padding-top: 90px;
+  padding-bottom: 50px;
+  .wap-nav {
+    position: fixed;
+    right: 0;
+    line-height: 44px;
+    background: #fff;
+  }
+}
+
+/* 在有作用域样式的组件中：默认只能对子组件的根节点样式生效 */
+
+// .vue 文件中有一个专有的特殊语法：让样式作用的更深（主要针对的子组件）
+// 使用 >>>、/deep/、::v-deep
+::v-deep .van-tabs__wrap {
+  position: fixed;
+  top: 46px;
+  left: 0;
+  right: 0;
+  z-index: 2;
+}
 
 </style>
